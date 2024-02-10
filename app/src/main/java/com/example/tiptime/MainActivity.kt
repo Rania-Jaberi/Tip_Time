d9def13c3c43c5f843e933ec1d4df4d3d537f2e4
Rania-Jaberi <raniajaberi31@gmail.com>
2024-02-10 16:18:46 +0100

    Une valeur calculée par la fonction remember est stockée dans la composition lors de la composition initiale, et la valeur stockée est renvoyée lors de la recomposition. L'ajout des importations "getter" et "setter" du délégué vous permet de lire et de définir amountInput sans faire référence à la propriété value de MutableState.

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -15,15 +15,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tiptime.ui.theme.TipTimeTheme
@@ -72,11 +79,16 @@ fun TipTimeLayout(){
    }
 }
 @Composable
-fun EditNumberField(modifier: Modifier = Modifier){
+fun EditNumberField(modifier: Modifier = Modifier) {
+    // Creating a mutable state object directly inside the composable function
+    var amountInput by remember { mutableStateOf("") }
 
-    TextField(value = "",
-              onValueChange = {},
-        modifier = modifier)
+    TextField(
+        value = amountInput,
+        onValueChange = { amountInput = it },
+        modifier = modifier,
+        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number)
+    )
 }
 
 private fun CalculateTip(amount: Double, tipPercent: Double = 15.0): String{
