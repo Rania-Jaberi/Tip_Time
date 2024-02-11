1db653b093000f7fb086ebcaa8c7dc98cc328f99
Rania-Jaberi <raniajaberi31@gmail.com>
2024-02-11 12:30:56 +0100

    Mettre à jour la fonction calculateTip() pour arrondir le pourboire: Dans la fonction calculateTip(), avant l'instruction return, ajoutez une condition if() qui vérifiera la valeur roundUp.

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -65,7 +65,7 @@ fun TipTimeLayout() {
     val amount = amountInput.toDoubleOrNull() ?: 0.0
     val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
 
-    val tip = calculateTip(amount, tipPercent)
+    val tip = calculateTip(amount, tipPercent, roundUp)
 
     Column(
         modifier = Modifier
@@ -168,8 +168,15 @@ fun EditNumberField(
 }
 
 
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
-    val tip = tipPercent / 100 * amount
+private fun calculateTip(
+    amount: Double,
+    tipPercent: Double = 15.0,
+    roundUp: Boolean
+): String {
+    var tip = tipPercent / 100 * amount
+    if (roundUp) {
+        tip = kotlin.math.ceil(tip)
+    }
     return NumberFormat.getCurrencyInstance().format(tip)
 }
 
